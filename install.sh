1142cd8ff7ccd62229360927cea2bccd8af5cbca
zephyros-dev <phong.ngthanh@outlook.com>
2022-06-15 14:13:21 +0700

    Add zerotier container

--- install.sh
+++ install.sh
@@ -20,14 +20,23 @@ brew install --cask \
     # Use rancher instead for better perfomance
     # docker \
 
+# Use amd64 images for best compatibility
+# touch ~/.bash_profile
+# grep -qxF 'export DOCKER_DEFAULT_PLATFORM=linux/amd64' ~/.bash_profile || echo \
+# 'export DOCKER_DEFAULT_PLATFORM=linux/amd64' >> ~/.bash_profile
+
+# Docker-compose plugin for docker
+mkdir -p ~/.docker/cli-plugins
+ln -sfn /opt/homebrew/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose
+
 # Personal
 brew install --cask \
     freetube \
     keepassxc \
-    # rectangle \
     syncthing \
     telegram \
     transmission-remote-gui \
+    zerotier-one \
 
 brew install \
     chezmoi \
@@ -35,7 +44,3 @@ brew install \
     docker-compose \
     fish \
     tmux \
-
-# Docker-compose plugin for docker
-mkdir -p ~/.docker/cli-plugins
-ln -sfn /opt/homebrew/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose
