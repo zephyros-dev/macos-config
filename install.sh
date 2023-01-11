998ad57532ff877900999253bd9f988bf1be5183
zephyros-dev <phong.ngthanh@outlook.com>
2023-01-11 09:48:08 +0700

    style: remove redundant comment

--- install.sh
+++ install.sh
@@ -5,10 +5,6 @@ then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     sudo softwareupdate --install-rosetta
 fi
-# Work software
-    # Choosing between rancher and docker desktop
-    # Docker-desktop has VirtioFS which improves bind mount perfomance significantly
-    # Rancher-desktop is open source but have some rough edges
 
 brew install --cask \
     anydesk \
@@ -21,15 +17,6 @@ brew install --cask \
     onlyoffice \
     thunderbird \
     visual-studio-code \
-
-# For installing Rancher-desktop
-# brew install --cask rancher \
-# && brew install docker docker-compose \
-
-# Use amd64 images for best compatibility
-# touch ~/.bash_profile
-# grep -qxF 'export DOCKER_DEFAULT_PLATFORM=linux/amd64' ~/.bash_profile || echo \
-# 'export DOCKER_DEFAULT_PLATFORM=linux/amd64' >> ~/.bash_profile
 
 # Docker-compose plugin for docker
 mkdir -p ~/.docker/cli-plugins
